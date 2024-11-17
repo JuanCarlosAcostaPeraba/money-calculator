de37815634f2da952f5435b3ff9c49c3ea13ee4b
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-11-17 14:48:27 +0000

    Update FixerAPI class to include a comment for educational purposes

--- src/main/java/software/ulpgc/money/calculator/fixerws/FixerAPI.java
+++ src/main/java/software/ulpgc/money/calculator/fixerws/FixerAPI.java
@@ -4,5 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class FixerAPI {
     private static final Dotenv dotenv = Dotenv.load();
+    // This line is for the teacher only
+    // public static final String key = "3595971b91355c2b5c019cab471b8dbd";
     public static final String key = dotenv.get("FIXER_API_KEY");
 }
